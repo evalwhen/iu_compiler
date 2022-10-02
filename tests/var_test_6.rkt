4e2f4635be58154478655eaa744daa3440627d8b
melp <huzi.master@outlook.com>
2022-10-02 19:27:23 +0800

    comment result.

--- tests/var_test_6.rkt
+++ tests/var_test_6.rkt
@@ -1,12 +1,12 @@
 (let ([x (+ 42 (- 10))])
   (+ x 10))
 
-(Program
- '()
- (Let
-  'g12956
-  (Let
-   'g12957
-   (Prim '- (list (Int 10)))
-   (Prim '+ (list (Int 42) (Var 'g12957))))
-  (Prim '+ (list (Var 'g12956) (Int 10)))))
+;; (Program
+;;  '()
+;;  (Let
+;;   'g12956
+;;   (Let
+;;    'g12957
+;;    (Prim '- (list (Int 10)))
+;;    (Prim '+ (list (Int 42) (Var 'g12957))))
+;;   (Prim '+ (list (Var 'g12956) (Int 10)))))
